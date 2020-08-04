d7b57df56264a3dd67c0f80f99a5675144e95f5a
Filip Matynia <koncepcjonista37@outlook.com>
2020-08-04 13:39:41 +0200

    Solution

---
+++ Simulatica/CalcEngine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalcEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
