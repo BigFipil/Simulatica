e8348a3b77832f0e3aba387d9aa08507d5cd5f3f
Filip Matynia <koncepcjonista37@outlook.com>
2021-04-05 19:45:50 +0200

    Minimalize, Maximalize and Exit

---
+++ Simulatica/Simulatica/User Controls/ExitBar.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Simulatica.User_Controls
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ExitBar.xaml
+    /// </summary>
+    public partial class ExitBar : UserControl
+    {
+        public ExitBar()
+        {
+            InitializeComponent();
+        }
+
+        void MinimalizeAction(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+
+            window.WindowState = WindowState.Minimized;
+        }
+
+        void MaximalizeAction(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+
+            if (window.WindowState != WindowState.Maximized)
+                window.WindowState = WindowState.Maximized;
+            else
+                window.WindowState = WindowState.Normal;
+        }
+
+        void Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+    }
+}
